1ba88df282ee6b23cd62ec33aeca607690e46f87
simahao <simahao@126.com>
2026-03-22 16:25:54 +0800

    feat: init

---
+++ src/test/java/hz/lift/LiftApplicationTests.java
@@ -0,0 +1,13 @@
+package hz.lift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
